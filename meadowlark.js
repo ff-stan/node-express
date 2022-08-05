0c3958b45894c612ef8078ff3153de1f76269255
lobi <5371072+gdsjys@user.noreply.gitee.com>
2022-08-05 13:51:57 +0800

    添加了测试单元与集成测试,设置了ESLint

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -10,30 +10,30 @@ app.engine('handlebars', expressHandlebars({
 }))
 app.set('view engine', 'handlebars')
 
+// Express会自动识别以下两个参数
+// 所以这里需要禁用no-undef规则
+/* eslint-disable no-undef */
 app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
+/* eslint-disable no-undef */
+
 
 // 设置路由
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about',{ fortune: fortune.getFortune() })
-})
+app.get('/about',handlers.about)
 
 // 定制404页
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 // 定制500页
-app.use((err, req, res, next) => {
-    console.log(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-    `Express started on http://localhost:${port};` +
-    `press Ctrl-C to terminate.`))
+if(require.main === module){
+    app.listen(port, () => console.log(
+        `Express started on http://localhost:${port};` +
+        `press Ctrl-C to terminate.`))
+}else{
+    module.exports = app
+}
